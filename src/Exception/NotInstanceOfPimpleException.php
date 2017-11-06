9b502bed5b9f542837757478f3352931803179d3
Nicholas English <awixe443@gmail.com>
2017-11-06 06:51:21 -0500

    Update NotInstanceOfPimpleException.php
    Add the constructor.

--- src/Exception/NotInstanceOfPimpleException.php
+++ src/Exception/NotInstanceOfPimpleException.php
@@ -2,5 +2,8 @@
 namespace Awixe\Container\Exception;
 use Psr\Container\NotFoundExceptionInterface;
 class NotInstanceOfPimpleException extends \InvalidArgumentException implements NotFoundExceptionInterface {
+    function __construct($container) {
+        parent::__construct(sprintf('Invalid instance of pimple. `%s`', var_export($container)));
+    }
 }
 ?>
