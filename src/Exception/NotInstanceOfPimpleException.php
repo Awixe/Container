1571b2dc7949ff57804edbb64c5e6164c0f3c6aa
Nicholas English <awixe443@gmail.com>
2017-11-06 11:59:31 +0000

    Apply fixes from StyleCI

--- src/Exception/NotInstanceOfPimpleException.php
+++ src/Exception/NotInstanceOfPimpleException.php
@@ -4,17 +4,21 @@
  *
  * GNU GENERAL PUBLIC LICENSE
  * Version 3, 29 June 2007
- * 
+ *
  * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
  * Everyone is permitted to copy and distribute verbatim copies
  * of this license document, but changing it is not allowed.
  *
  */
+
 namespace Awixe\Container\Exception;
+
 use Psr\Container\NotFoundExceptionInterface;
-class NotInstanceOfPimpleException extends \InvalidArgumentException implements NotFoundExceptionInterface {
-    function __construct($container) {
+
+class NotInstanceOfPimpleException extends \InvalidArgumentException implements NotFoundExceptionInterface
+{
+    public function __construct($container)
+    {
         parent::__construct(sprintf('Invalid instance of pimple. `%s`', var_export($container)));
     }
 }
-?>
