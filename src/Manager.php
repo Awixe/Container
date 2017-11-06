70f84a1fd42cc1eda3ab7522b5813a562da7f14a
Nicholas English <awixe443@gmail.com>
2017-11-06 08:26:14 -0500

    Update Manager.php

--- src/Manager.php
+++ src/Manager.php
@@ -15,15 +15,39 @@
 
 namespace Awixe\Container;
 
+use Awixe\Container\Exception\InvalidBaseConfiguration;
 use Awixe\Container\Exception\InvalidServiceReference;
+use Pimple\Container as PimpleContainer;
 use Traversable;
+
+define('ROOT_APPLICATION', __DIR__  . '/../../../../');
 
 class Manager extends Container implements ManagerInterface
 {
-    public function __invoke($servicesToCall = [])
+    public function __invoke($servicesToCall = [], array $baseConfiguration = array())
     {
-        if (!($servicesToCall instanceof Traversable) && !is_array($servicesToCall)) {
+        if (!($servicesToCall instanceof Traversable || is_array($servicesToCall))) {
             throw new InvalidServiceReference($servicesToCall);
         }
+        $container = new PimpleContainer();
+        if (empty($baseConfiguration)) {
+            throw new InvalidBaseConfigurationException('No data was provided.');
+        }
+        $services = isset($baseConfiguration['services'] ? $baseConfiguration['services'] : null;
+        $customPath = isset($baseConfiguration['servicesPath']) ? $baseConfiguration['servicesPath'] : '';
+        if (!$services) {
+            throw new InvalidBaseConfigurationException('No services to call.');
+        }
+        if (!is_string($customPath)) {
+            throw new InvalidBaseConfigurationException('Invalid path location.');
+        }
+        foreach ($services as $service) {
+            if (!is_string($service)) {
+                throw new InvalidBaseConfigurationException('Array values need to be passed as strings.');
+            }
+            require_once ROOT_APPLICATION . rtrim($custompath, '/\\') . "/{$service}.php";
+        }
+      
+        
     }
 }
