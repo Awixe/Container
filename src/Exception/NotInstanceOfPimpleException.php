38c6af15b00831faa2f07b957ee9b74d71d567f3
Nicholas English <awixe443@gmail.com>
2017-11-06 06:59:25 -0500

    Add license / Comment header.

--- src/Exception/NotInstanceOfPimpleException.php
+++ src/Exception/NotInstanceOfPimpleException.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is part of Awixe/Container
+ *
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ * 
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ */
 namespace Awixe\Container\Exception;
 use Psr\Container\NotFoundExceptionInterface;
 class NotInstanceOfPimpleException extends \InvalidArgumentException implements NotFoundExceptionInterface {
