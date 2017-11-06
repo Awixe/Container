02a7713e3cd6f4e9a49318fe05661a82bb662b9e
Nicholas English <awixe443@gmail.com>
2017-11-06 07:12:18 -0500

    Update Container.php
    Add the class that initializes the container from Pimple.

--- src/Container.php
+++ src/Container.php
@@ -1,1 +1,32 @@
-
+<?php
+/*
+ * This file is part of Awixe/Container
+ *
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ * 
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ */
+namespace Awixe/Container;
+use Awixe\Exception\NotInstanceOfPimpleException;
+use Pimple\Container as PimpleContainer;
+class Container {
+    protected static $instance;
+    protected static function initialize(string $service = null) {
+	      if (!(self::$instance instanceof PimpleContainer)) {
+		        throw new NotInstanceOfPimpleException(self::$container);
+        }
+        $container = self::$instance;
+	      if (is_null($service)) {
+            return $container;
+        } elseif (!isset($container[$service])) {
+            throw new UnknownService($service);
+        } else {
+            return $container[$service];
+        }
+    }
+}
+?>
