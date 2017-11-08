95ec6acdd15197abb682c054dbcdd1bc48ffb94f
Nicholas English <awixe443@gmail.com>
2017-11-07 19:16:06 -0500

    Update NotInstanceOfPimpleExceptionTest.php

--- tests/NotInstanceOfPimpleExceptionTest.php
+++ tests/NotInstanceOfPimpleExceptionTest.php
@@ -1,1 +1,18 @@
-
+<?php
+/*
+ * This file is part of Awixe/Container
+ *
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ * <https://github.com/Awixe/Container/blob/master/LICENSE>
+ *
+ */
+namespace Awixe\Container\Tests;
+use PHPUnit\Framework\TestCase;
+final class NotInstanceOfPimpleExceptionTest extends TestCase {
+}
